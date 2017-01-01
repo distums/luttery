02543d39396b9d5b000c86e81ca7ad29320e7315
distums <codeqilin@hotmail.com>
2017-01-01 22:38:04 +0800

    remove meterial menu

--- index.js
+++ index.js
@@ -2,53 +2,28 @@
  * Created by distu on 2016/12/19.
  */
 const users = (window.users || []).map((name,index) => ({name, isReward: false,id:index}));
-const USERS_COUNT_PER_ROW = 18;
+const USERS_COUNT_PER_ROW = 25;
 
 var camera, scene, renderer;
 var controls;
 var objects = [];
 var targets = { table: [], sphere: [], helix: [], grid: [] };
 
-function renderUser(user) {
-  return (`<div class="demo-card-image mdl-card mdl-shadow--2dp"
-              data-user="${user.name}">
-            <div class="mdl-card__title mdl-card--expand">LU</div>
-          </div>`);
-}
-
-function renderCheckBox(id,checked) {
-  return (`
-    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-${id}">
-      <input type="checkbox" id="checkbox-${id}" class="mdl-checkbox__input" ${checked?"checked":""}>
-    </label>
-  `);
-}
-
-function renderUserTableRow(user) {
-  return (`
-    <tr>
-      <td class="mdl-data-table__cell--non-numeric">${user.id + 1}</td>
-      <td class="mdl-data-table__cell--non-numeric">${user.name}</td>
-      <td class="mdl-data-table__cell--non-numeric">${renderCheckBox(user.id,user.isReward)}</td>
-    </tr>
-  `);
-}
-
-function compose(...fns) {
-  const actualFns = fns.filter(fn => typeof fn === 'function');
-  return (data) => actualFns.reduceRight((result, f) => f(result), data);
-}
-
 function init() {
   camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 2500;
+  camera.position.z = 2800;
   scene = new THREE.Scene();
   // table
   for ( let i = 0; i < users.length; i++ ) {
     const element = document.createElement( 'div' );
-    element.className = 'demo-card-image mdl-card mdl-shadow--2dp';
+    element.className = 'element';
+    element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
+    const number = document.createElement( 'div' );
+    number.className = 'number';
+    number.textContent = users[i].id;
+    element.appendChild( number );
     const symbol = document.createElement( 'div' );
-    symbol.className = 'mdl-card__title mdl-card--expand';
+    symbol.className = 'symbol';
     symbol.textContent = users[i].name;
     element.appendChild( symbol );
     const object = new THREE.CSS3DObject( element );
@@ -59,7 +34,7 @@ function init() {
     objects.push( object );
     //
     const objectForTable = new THREE.Object3D();
-    objectForTable.position.x = ( (i % USERS_COUNT_PER_ROW + 1) * 140 ) - 1330;
+    objectForTable.position.x = ( (i % USERS_COUNT_PER_ROW + 1) * 140 ) - 1840;
     objectForTable.position.y = - ((Math.floor(i / USERS_COUNT_PER_ROW) + 1) * 180 ) + 990;
     targets.table.push( objectForTable );
   }
@@ -81,7 +56,7 @@ function init() {
   var cylindrical = new THREE.Cylindrical();
   for ( var i = 0, l = objects.length; i < l; i ++ ) {
     var theta = i * 0.175 + Math.PI;
-    var y = - ( i * 8 ) + 450;
+    var y = - ( i * 8 ) + 650;
     var object = new THREE.Object3D();
     cylindrical.set( 900, theta, y );
     object.position.setFromCylindrical( cylindrical );
@@ -171,7 +146,4 @@ function render() {
 document.addEventListener('DOMContentLoaded', ()=> {
   init();
   animate();
-
-  const userTable = document.querySelector('#user-table');
-  userTable.querySelector('tbody').innerHTML = users.map(renderUserTableRow).join('\r\n');
 });
